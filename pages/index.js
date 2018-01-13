d717d46b4188e68152e6bce15060c9590098fdde
Andres Martin <andres.fernando.martin.martin@gmail.com>
2018-01-13 20:26:28 +0900

    Fetch forecast for given location and display in Cards

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,51 @@
 import React from 'react'
 import Head from 'next/head'
-import { Input } from 'semantic-ui-react'
+import fetch from 'isomorphic-unfetch'
+import url from 'url'
+
+import { Card, Input } from 'semantic-ui-react'
 
 export default class Index extends React.Component {
   constructor (props) {
     super(props)
-    this.state = { location: '' }
+    this.state = {
+      location: '',
+      forecast: []
+    }
     this.onLocationChange = this.onLocationChange.bind(this)
     this.onLocationSearch = this.onLocationSearch.bind(this)
   }
 
   onLocationChange (event, data) {
     this.setState({
-      location: data.value
+      location: data.value,
+      forecast: []
     })
   }
 
   onLocationSearch () {
-    console.log('Search weather at', this.state.location, 'using Yahoo Weather API')
+    console.log(`Search weather at "${this.state.location}" using Yahoo Weather API`)
+    const apiUrlObject = {
+      protocol: 'https',
+      hostname: 'query.yahooapis.com',
+      pathname: 'v1/public/yql',
+      query: {
+        q: `select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="${this.state.location}")`,
+        format: 'json',
+        env: 'http://datatables.org/alltables.env'
+      }
+    }
+    fetch(url.format(apiUrlObject))
+      .then(response => response.json())
+      .then(data => {
+        if (data.query.results) {
+          this.setState({
+            forecast: data.query.results.channel.item.forecast
+          })
+        } else {
+          console.log(`No results found for location search "${this.state.location}"`)
+        }
+      })
   }
 
   render () {
@@ -30,6 +58,27 @@ export default class Index extends React.Component {
         <h1 className='title'>Weather SPA</h1>
         <p>Location</p>
         <Input placeholder='Eg. Tokyo' onChange={this.onLocationChange} action={{ content: 'Search', onClick: this.onLocationSearch }} />
+        <Card.Group itemsPerRow={5}>
+          {
+            this.state.forecast.map(dayForecast => (
+              <Card>
+                <Card.Content>
+                  <Card.Header>
+                    { dayForecast.text }
+                  </Card.Header>
+                  <Card.Meta>
+                    <span className='date'>
+                      { dayForecast.date }
+                    </span>
+                  </Card.Meta>
+                  <Card.Description>
+                    High { dayForecast.high }° - Low { dayForecast.low }°
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            ))
+          }
+        </Card.Group>
         <style jsx>{`
           .container {
             width: 1200px;
