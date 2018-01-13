99338d61234d804bdf2bc2e6dc46e07e989283dd
Andres Martin <andres.fernando.martin.martin@gmail.com>
2018-01-13 21:29:04 +0900

    Add support to display temperature in different units

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,13 @@ import Head from 'next/head'
 import fetch from 'isomorphic-unfetch'
 import url from 'url'
 
-import { Card, Input } from 'semantic-ui-react'
+import { Button, Card, Input } from 'semantic-ui-react'
 
 export default class Index extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
+      unit: 'fahrenheit',
       location: '',
       forecast: []
     }
@@ -48,6 +49,16 @@ export default class Index extends React.Component {
       })
   }
 
+  onChangeTempUnit (unit) {
+    this.setState({
+      unit: unit
+    })
+  }
+
+  FtoC (temp) {
+    return Math.round((temp - 32) / (9 / 5))
+  }
+
   render () {
     return (
       <div className='container'>
@@ -56,36 +67,69 @@ export default class Index extends React.Component {
           <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css' />
         </Head>
         <h1 className='title'>Weather SPA</h1>
-        <p>Location</p>
-        <Input placeholder='Eg. Tokyo' onChange={this.onLocationChange} action={{ content: 'Search', onClick: this.onLocationSearch }} />
-        <Card.Group itemsPerRow={5}>
-          {
-            this.state.forecast.map(dayForecast => (
-              <Card>
-                <Card.Content>
-                  <Card.Header>
-                    { dayForecast.text }
-                  </Card.Header>
-                  <Card.Meta>
-                    <span className='date'>
-                      { dayForecast.date }
-                    </span>
-                  </Card.Meta>
-                  <Card.Description>
-                    High { dayForecast.high }° - Low { dayForecast.low }°
-                  </Card.Description>
-                </Card.Content>
-              </Card>
-            ))
-          }
-        </Card.Group>
+        <div className='unit'>
+          <Button.Group>
+            <Button color={this.state.unit === 'fahrenheit' ? 'blue' : null} onClick={() => this.onChangeTempUnit('fahrenheit')}>Fahrenheit</Button>
+            <Button.Or />
+            <Button color={this.state.unit === 'celsius' ? 'blue' : null} onClick={() => this.onChangeTempUnit('celsius')}>Celsius</Button>
+          </Button.Group>
+        </div>
+        <div className='location'>
+          <p>Location</p>
+          <Input placeholder='Eg. Tokyo' onChange={this.onLocationChange} action={{ content: 'Search', onClick: this.onLocationSearch }} />
+        </div>
+        <div className='forecast'>
+          <Card.Group itemsPerRow={5}>
+            {
+              this.state.forecast.map(dayForecast => (
+                <Card>
+                  <Card.Content>
+                    <Card.Header>
+                      { dayForecast.text }
+                    </Card.Header>
+                    <Card.Meta>
+                      <span className='date'>
+                        { dayForecast.date }
+                      </span>
+                    </Card.Meta>
+                    <Card.Description>
+                      {
+                        this.state.unit === 'fahrenheit'
+                          ? <span>High { dayForecast.high }°F - Low { dayForecast.low }°F</span>
+                          : <span>High { this.FtoC(dayForecast.high) }°C - Low { this.FtoC(dayForecast.low) }°C</span>
+                      }
+                    </Card.Description>
+                  </Card.Content>
+                </Card>
+              ))
+            }
+          </Card.Group>
+        </div>
+        <div className='footer'>
+          <a href='https://www.yahoo.com/?ilc=401' target='_blank'>
+            <img src='https://poweredby.yahoo.com/purple.png' width='134' height='29' />
+          </a>
+        </div>
         <style jsx>{`
           .container {
             width: 1200px;
             margin: 0 auto;
+            padding: 20px 0;
           }
           .title {
-            margin: 10px 0;
+            margin-bottom: 20px;
+          }
+          .unit {
+            float: right;
+          }
+          .location {
+            margin-bottom: 20px;
+          }
+          .forecast {
+            margin-bottom: 20px;
+          }
+          .footer {
+            margin-bottom: 20px;
           }
         `}</style>
       </div>
